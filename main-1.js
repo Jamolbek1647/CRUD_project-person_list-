355becbf417e2ddb2358c3f2d6ca0dc5adb7f114
Jamolbek1647 <jamolbekikromov@mail.com>
2023-02-10 01:34:26 +0500

    addItem and print function added

---
+++ main-1.js
@@ -0,0 +1,42 @@
+class Person {
+    constructor(firstname, lastname, age, address) {
+        this.firstname = firstname
+        this.lastname = lastname
+        this.age = age
+        this.address = address
+    }
+}
+
+class Address {
+    constructor(region, district, street, apartment) {
+        this.region = region
+        this.district = district
+        this.street = street
+        this.apartment = apartment
+    }
+}
+let persons = []
+function addItem() {
+    let firstname = document.getElementById('firstname')
+    let lastname = document.getElementById('lastname')
+    let age = document.getElementById('age')
+    let region = document.getElementById('region')
+    let district = document.getElementById('district')
+    let street = document.getElementById('street')
+    let apartment = document.getElementById('apartment')
+    persons.push(new Person(firstname, lastname, age, new Address(region, district, street, apartment)))
+    return persons
+}
+
+function print(){
+    let response = document.getElementById('print')
+    return response.value = persons
+}
+
+function SynctoLocalStorage(){
+    localStorage.setItem('persons', JSON.stringify(persons))
+}
+
+function SyncfromLocalStorage(){
+    const get_from_local = localStorage.getItem('persons')
+}
